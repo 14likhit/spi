830db0089644a0de424f74685af79841c4555e17
14likhit <jaijalaram@4495BCU>
2021-05-21 17:01:00 +0530

    - Added data binding for the activities.

--- app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
@@ -1,12 +1,18 @@
 package com.amotrade.spi.ui.onboarding
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.amotrade.spi.R
 import com.amotrade.spi.base.BaseActivity
+import com.amotrade.spi.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : BaseActivity() {
+
+    private lateinit var activityOnboardingBinding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        activityOnboardingBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_onboarding)
     }
 }
--- app/src/main/java/com/amotrade/spi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/amotrade/spi/ui/splash/SplashActivity.kt
@@ -1,12 +1,17 @@
 package com.amotrade.spi.ui.splash
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.amotrade.spi.R
 import com.amotrade.spi.base.BaseActivity
+import com.amotrade.spi.databinding.ActivitySplashBinding
 
 class SplashActivity : BaseActivity() {
+
+    private lateinit var activitySplashBinding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        activitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
     }
 }
