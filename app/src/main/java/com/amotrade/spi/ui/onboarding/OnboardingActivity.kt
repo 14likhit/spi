cc2c8df1faecd4b054549a39c88459655d5ac8d6
14likhit <jaijalaram@4495BCU>
2021-05-21 16:48:33 +0530

    - Added required Activities.

---
+++ app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.amotrade.spi.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.amotrade.spi.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
--- app/src/main/java/com/amotrade/spi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/amotrade/spi/ui/splash/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.amotrade.spi
+package com.amotrade.spi.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.amotrade.spi.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
