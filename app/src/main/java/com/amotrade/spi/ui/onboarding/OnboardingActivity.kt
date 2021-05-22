ef90fce78693951b9df47e32f7a9fb6dbdd0cc28
14likhit <jaijalaram@4495BCU>
2021-05-22 16:16:33 +0530

    - Added view model in Activites.

--- app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
@@ -2,17 +2,34 @@ package com.amotrade.spi.ui.onboarding
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.amotrade.spi.R
 import com.amotrade.spi.base.BaseActivity
 import com.amotrade.spi.databinding.ActivityOnboardingBinding
+import com.amotrade.spi.viewmodelfactory.OnboardingViewModelFactory
+import com.amotrade.spi.viewmodels.OnboardingViewModel
 
 class OnboardingActivity : BaseActivity() {
 
     private lateinit var activityOnboardingBinding: ActivityOnboardingBinding
 
+    private lateinit var onboardingViewModelFactory: OnboardingViewModelFactory
+    private lateinit var onboardingViewModel: OnboardingViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityOnboardingBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_onboarding)
+
+        onboardingViewModelFactory = OnboardingViewModelFactory()
+
+        onboardingViewModel =
+            ViewModelProvider(this, onboardingViewModelFactory).get(OnboardingViewModel::class.java)
+
+        initView()
+    }
+
+    private fun initView() {
+
     }
 }
--- app/src/main/java/com/amotrade/spi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/amotrade/spi/ui/splash/SplashActivity.kt
@@ -2,16 +2,32 @@ package com.amotrade.spi.ui.splash
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.amotrade.spi.R
 import com.amotrade.spi.base.BaseActivity
 import com.amotrade.spi.databinding.ActivitySplashBinding
+import com.amotrade.spi.viewmodelfactory.SplashViewModelFactory
+import com.amotrade.spi.viewmodels.SplashViewModel
 
 class SplashActivity : BaseActivity() {
 
     private lateinit var activitySplashBinding: ActivitySplashBinding
 
+    private lateinit var splashViewModelFactory: SplashViewModelFactory
+    private lateinit var splashViewModel: SplashViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activitySplashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        splashViewModelFactory = SplashViewModelFactory()
+        splashViewModel =
+            ViewModelProvider(this, splashViewModelFactory).get(SplashViewModel::class.java)
+
+        intiView()
+    }
+
+    private fun intiView() {
+
     }
 }
