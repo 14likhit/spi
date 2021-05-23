a510163f381fed0c2f5545ae6270a5be14567c18
14likhit <jaijalaram@4495BCU>
2021-05-23 17:19:39 +0530

    - Added Home Activity.

---
+++ app/src/main/java/com/amotrade/spi/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.amotrade.spi.ui.home
+
+import android.os.Bundle
+import com.amotrade.spi.R
+import com.amotrade.spi.base.BaseActivity
+
+class HomeActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
