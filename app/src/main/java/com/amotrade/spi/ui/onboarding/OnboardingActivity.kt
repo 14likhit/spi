f8f4c82ac5d3fec4918229fb5d8435270a83ae49
14likhit <jaijalaram@4495BCU>
2021-05-23 16:54:00 +0530

    - Updated theme for the app. - Updated logic Onboarding activity.

--- app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/amotrade/spi/ui/onboarding/OnboardingActivity.kt
@@ -1,11 +1,15 @@
 package com.amotrade.spi.ui.onboarding
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.amotrade.spi.R
 import com.amotrade.spi.base.BaseActivity
 import com.amotrade.spi.databinding.ActivityOnboardingBinding
+import com.amotrade.spi.utils.isTextEnteredEmpty
 import com.amotrade.spi.viewmodelfactory.OnboardingViewModelFactory
 import com.amotrade.spi.viewmodels.OnboardingViewModel
 
@@ -15,6 +19,9 @@ class OnboardingActivity : BaseActivity() {
 
     private lateinit var onboardingViewModelFactory: OnboardingViewModelFactory
     private lateinit var onboardingViewModel: OnboardingViewModel
+
+    private var panCardNumber = ""
+    private var dob = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +33,131 @@ class OnboardingActivity : BaseActivity() {
         onboardingViewModel =
             ViewModelProvider(this, onboardingViewModelFactory).get(OnboardingViewModel::class.java)
 
+        setObservers()
+
         initView()
     }
 
-    private fun initView() {
+    private fun setObservers() {
+        onboardingViewModel.getIsValidFormMutableLiveData().observe(this, {
+            if (it != null && it) {
+                setNextButtonEnable(true)
+            }
+        })
 
+        onboardingViewModel.getErrorPanNumberMutableLiveData().observe(this, {
+            if (it != null && it) {
+                setErrorPanNumber()
+            }
+        })
+
+        onboardingViewModel.getErrorDateOfBirthMutableLiveData().observe(this, {
+            if (it != null && it) {
+                setErrorDateOfBirth()
+            }
+        })
+    }
+
+    private fun initView() {
+        setNextButtonEnable(false)
+
+        setTextWatchers()
+
+        activityOnboardingBinding.nextBtn.setOnClickListener {
+            setFormDetails()
+            onboardingViewModel.validateForm(panCardNumber, dob)
+        }
+    }
+
+    private fun setFormDetails() {
+        panCardNumber = activityOnboardingBinding.panEt.text.toString()
+        dob = activityOnboardingBinding.birthdateDateEt.text.toString() + "/" +
+                activityOnboardingBinding.birthdateMonthEt.text.toString() + "/" +
+                activityOnboardingBinding.birthdateYearEt.text.toString()
+    }
+
+    private fun setNextButtonEnable(isButtonEnabled: Boolean) {
+        activityOnboardingBinding.nextBtn.isEnabled = isButtonEnabled
+        activityOnboardingBinding.nextBtn.backgroundTintList = ColorStateList.valueOf(
+            resources.getColor(
+                if (isButtonEnabled) {
+                    R.color.purple_500
+                } else {
+                    R.color.purple_200
+                }
+            )
+        )
+    }
+
+    private fun setTextWatchers() {
+        activityOnboardingBinding.panEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                activityOnboardingBinding.panInputTil.isErrorEnabled = false
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(editable: Editable?) {
+                checkNextButtonToEnable()
+            }
+        })
+
+        activityOnboardingBinding.birthdateDateEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                activityOnboardingBinding.errorDateOfBirth.text = ""
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(editable: Editable?) {
+                checkNextButtonToEnable()
+            }
+        })
+
+        activityOnboardingBinding.birthdateMonthEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                activityOnboardingBinding.errorDateOfBirth.text = ""
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(editable: Editable?) {
+                checkNextButtonToEnable()
+            }
+        })
+
+        activityOnboardingBinding.birthdateYearEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                activityOnboardingBinding.errorDateOfBirth.text = ""
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(editable: Editable?) {
+                checkNextButtonToEnable()
+            }
+        })
+
+
+    }
+
+    private fun checkNextButtonToEnable() {
+        if (isTextEnteredEmpty(activityOnboardingBinding.panEt) ||
+            isTextEnteredEmpty(activityOnboardingBinding.birthdateDateEt) ||
+            isTextEnteredEmpty(activityOnboardingBinding.birthdateMonthEt) ||
+            isTextEnteredEmpty(activityOnboardingBinding.birthdateYearEt)
+        ) {
+            setNextButtonEnable(false)
+        } else {
+            setNextButtonEnable(true)
+        }
+    }
+
+    private fun setErrorPanNumber() {
+        activityOnboardingBinding.panInputTil.error = getString(R.string.error_pan)
+    }
+
+    private fun setErrorDateOfBirth() {
+        activityOnboardingBinding.errorDateOfBirth.text = getString(R.string.error_birthdate)
     }
 }
--- app/src/main/java/com/amotrade/spi/viewmodels/OnboardingViewModel.kt
+++ app/src/main/java/com/amotrade/spi/viewmodels/OnboardingViewModel.kt
@@ -1,12 +1,68 @@
 package com.amotrade.spi.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.amotrade.spi.utils.BIRTHDATE_FORMAT
+import com.amotrade.spi.utils.STRING_PATTERN_PAN
 import io.reactivex.disposables.CompositeDisposable
+import java.text.ParseException
+import java.util.regex.Pattern
 
 class OnboardingViewModel() : ViewModel() {
 
     private val disposables = CompositeDisposable()
+    private val errorPanNumberMutableLiveData = MutableLiveData<Boolean>()
+    private val errorDateOfBirthMutableLiveData = MutableLiveData<Boolean>()
+    private val isValidFormMutableLiveData = MutableLiveData<Boolean>()
 
+    fun getErrorPanNumberMutableLiveData(): MutableLiveData<Boolean> {
+        return errorPanNumberMutableLiveData
+    }
+
+    fun getErrorDateOfBirthMutableLiveData(): MutableLiveData<Boolean> {
+        return errorDateOfBirthMutableLiveData
+    }
+
+    fun getIsValidFormMutableLiveData(): MutableLiveData<Boolean> {
+        return isValidFormMutableLiveData
+    }
+
+    private fun isValidPanNumber(panNumber: String): Boolean {
+        val pattern = Pattern.compile(STRING_PATTERN_PAN)
+        val matcher = pattern.matcher(panNumber)
+        return matcher.matches()
+    }
+
+    private fun isValidDateOfBirth(dateOfBirth: String): Boolean {
+        val dateArray = dateOfBirth.split("/")
+        val date: Int = dateArray[0].toInt()
+        val month: Int = dateArray[1].toInt()
+        val year: Int = dateArray[2].toInt()
+        if (date > 31) {
+            return false
+        } else if (month > 12) {
+            return false
+        } else if (year < 1960 && year > 2011) {
+            return false
+        } else {
+            return try {
+                val date = BIRTHDATE_FORMAT.parse(dateOfBirth)
+                true
+            } catch (e: ParseException) {
+                false
+            }
+        }
+    }
+
+    fun validateForm(panNumber: String, dateOfBirth: String) {
+        if (isValidPanNumber(panNumber) && isValidDateOfBirth(dateOfBirth)) {
+            isValidFormMutableLiveData.value = true
+        } else if (!isValidPanNumber(panNumber)) {
+            errorPanNumberMutableLiveData.value = true
+        } else {
+            errorDateOfBirthMutableLiveData.value = true
+        }
+    }
 
     override fun onCleared() {
         disposables.clear()
